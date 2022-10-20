a07b25507363862ec8d03306c0eee0e374f50b08
allygonzalez <ag3959@drexel.edu>
2022-10-19 23:43:29 -0400

    scroll trigger!

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,14 +3,45 @@ import $ from "jquery";
 let $nav = $('nav[data-nav]');
 let isVisible = false;
 
-$(".nav-btns").on("click", function(){
+$(".nav-btns").on("click", function () {
     console.log("click");
-    if(isVisible === false){
+    if (isVisible === false) {
         $nav.show();
         isVisible = true;
-    }
-    else{
+    } else {
         $nav.hide();
         isVisible = false;
     }
 })
+import {
+    gsap
+} from "gsap";
+import {
+    ScrollTrigger
+} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#hero-2", {
+    duration: 2,
+    x: 0,
+    rotation: 360,
+    scrollTrigger: {
+        trigger: "#hero-2",
+        toggleActions: "restart pause reverse pause",
+        markers: true
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+});
