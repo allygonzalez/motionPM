bf2c928cb9f8a9e0b74cb0c6d4cff9734358d7e3
allygonzalez <ag3959@drexel.edu>
2022-11-30 17:14:41 -0500

    put in w

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,52 +1,30 @@
-import {
-        gsap
-} from "gsap";
-import {
-        GSDevTools
-} from "gsap/GSDevTools";
-import {
-        DrawSVGPlugin
-} from "gsap/DrawSVGPlugin";
-import {
-        MotionPathPlugin
-} from "gsap/MotionPathPlugin";
-
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin, MotionPathPlugin);
 
-
-
 function tealoopAnimation() {
-        var tl = gsap.timeline();
-        gsap.to("#tealoop"), {
-                duration: 5,
-                repeat: 12,
-                repeatDelay: 3,
-                yoyo: true,
-                ease: "power1.inOut",
-                motionPath: "#logopath",
-                align: "#logopath",
-                autoRotate: true,
-                alignOrigin: [0.5, 0.5]
-        }
-
-
-        return tl;
+  var tl = gsap.timeline();
+  tl.to("#tealoop",
+    {
+      duration: 2,
+      repeat: 12,
+      repeatDelay: 3,
+      ease: "power1.inOut",
+      motionPath:{
+        path: "#logopath",
+        align: "#logopath",
+        autoRotate: true,
+        alignOrigin: [0.5, 0.5]
 }
+    });
 
-
-
-
-
-
-
-
-
-
+  return tl;
+}
 
 var mainTL = gsap.timeline();
 mainTL.add(tealoopAnimation());
-
-
 
 GSDevTools.create();
